70aaaeeed515b244d47d6cca02415f8b0ee8cff1
aniciasantos <anicia@hey.com>
2025-07-22 16:41:59 -0700

    Update superset_config.py

--- superset_config.py
+++ superset_config.py
@@ -4,9 +4,9 @@ import os
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # Database configuration for Heroku Postgres
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace('postgres://', 'postgresql://')
+# SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace('postgres://', 'postgresql://')
 
 # Other Heroku-friendly settings
-WTF_CSRF_ENABLED = True
-SUPERSET_WEBSERVER_PORT = int(os.environ.get('PORT', 8088))
+# WTF_CSRF_ENABLED = True
+# SUPERSET_WEBSERVER_PORT = int(os.environ.get('PORT', 8088))
 
