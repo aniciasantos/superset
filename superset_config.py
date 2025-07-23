62fcf88665d559fdc6f7e2364afcb147ac43c08b
aes <aes@aess-MacBook-Air.local>
2025-07-22 17:31:48 -0700

    changed superset config for postgres

--- superset_config.py
+++ superset_config.py
@@ -4,11 +4,12 @@ import os
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DATABASE_URL = os.environ.get('DATABASE_URL')
-if DATABASE_URL and DATABASE_URL.startswith('postgres://'):
-    DATABASE_URL = DATABASE_URL.replace('postgres://', 'postgresql://', 1)
+if DATABASE_URL:
+    if DATABASE_URL.startswith('postgres://'):
+        DATABASE_URL = DATABASE_URL.replace('postgres://', 'postgresql://', 1)
+    SQLALCHEMY_DATABASE_URI = DATABASE_URL
+else:
+    raise Exception("DATABASE_URL environment variable not found")
 
-
-
-
-SQLALCHEMY_DATABASE_URI = DATABASE_URL
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+SECRET_KEY = os.environ.get('SECRET_KEY', 'change-this-to-something-secure')
